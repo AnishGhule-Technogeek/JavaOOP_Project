381f10d1ec327fdc80a6893d2d6de2962382d534
AnishGhule <anishghule12@gmail.com>
2021-11-24 23:40:13 +0530

    changes by al

--- src/poker/Player.java
+++ src/poker/Player.java
@@ -1,43 +1,115 @@
 package poker;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.lang.IllegalArgumentException;
 
 public class Player {
-    
+
     private String name;
-    boolean hasButton;
+    public boolean isCurrentPlayer;
     private boolean isActive;
-    private List<Card> cards;
-    
-    Player(){
-        this.name = "Anonymous";
-        this.hasButton = false;
-        this.isActive = false;
+    private ArrayList<Card> cards;
+    private int chips;
+    private boolean equalBet;
+    private int currentBet;
+
+    Player(String name, int chips) {
+        this.name = name;
+        this.isCurrentPlayer = false;
+        this.isActive = true;
         cards = new ArrayList<Card>();
+        this.chips = chips;
+        this.equalBet = false;
+        this.currentBet = 0;
     }
-    
+
     public String getName() {
         return name;
     }
-    
-    public void setName(String name) {
-        this.name = name;
-    }
-    
+
     public boolean isActive() {
         return isActive;
     }
-    
+
     public void setActive(boolean active) {
         isActive = active;
     }
-    
+
     public List<Card> getCards() {
         return cards;
     }
-    
+
     public void setCards(Card card) {
         this.cards.add(card);
+    }
+
+    public int getChips() {
+        return chips;
+    }
+
+    public boolean isEqualBet() {
+        return equalBet;
+    }
+
+    public void setEqualBet(boolean equalBet) {
+        this.equalBet = equalBet;
+    }
+
+    public int getCurrentBet() {
+        return currentBet;
+    }
+
+    public void setCurrentBet(int currentBet) {
+        this.currentBet = currentBet;
+    }
+
+    public void bet(int chips) throws IllegalArgumentException{
+        if (chips > this.chips) {
+            throw new IllegalArgumentException();
+        } else {
+            this.chips -= chips;
+            this.currentBet = chips;
+            this.equalBet = true;
+            System.out.println("Updated number of chips with you " + name + " : " + this.chips);
+        }
+    }
+
+    public void call(int chips) throws IllegalArgumentException{
+        if (chips > this.chips) {
+            throw new IllegalArgumentException();
+        } else {
+            this.chips -= chips;
+            this.currentBet = chips;
+            this.equalBet = true;
+            System.out.println("Updated number of chips with you " + name + " : " + this.chips);
+        }
+    }
+
+    // ANISH: Follwing is used in Table class for raise() method
+    public void raise(int chips){
+        // // FIXME: See if method is safetyChecked in Table Class itself
+        // while(chips < this.chips){
+        //     System.out.println("You must enter a higher amount!");
+        //     System.out.println("What's the final amount you want to bet? ");
+        //
+        //     Scanner sc = new Scanner(System.in);
+        //     chips = sc.nextInt();
+        //     raise(chips);
+        // }
+        //
+
+        if (chips > this.chips) {
+            throw new IllegalArgumentException();
+        } else {
+            this.chips -= chips;
+            this.currentBet = chips;
+            this.equalBet = true;
+            System.out.println("Updated number of chips:" + this.chips);
+        }
+    }
+
+    public void fold() {
+        System.out.printf("You have folded. Thus, you quit the game, %s\n\n", this.getName());
+        this.isActive = false;
     }
 }
